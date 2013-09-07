119e498929147eb4ced24c1d96e6645a09fd842c
Adrian Bordinc <adrian.bordinc@it-kartellet.dk>
2013-09-07 19:32:07 +0200

    Add File-Close, File-New

--- src/Stico/lib/core.js
+++ src/Stico/lib/core.js
@@ -7,40 +7,58 @@ var fs = require('fs');
 var win=gui.Window.get();
 var menubar=new gui.Menu({type: 'menubar'});
 var file=new gui.Menu();
+var actions = new gui.Menu();
+
 var openItem=new gui.MenuItem({label: 'Open'});
 var saveItem=new gui.MenuItem({label: 'Save'});
 var saveAsItem=new gui.MenuItem({label: 'Save As'});
+var newItem = new gui.MenuItem({label: 'New'});
+var closeItem = new gui.MenuItem({label: 'Close'});
+file.append(newItem);
 file.append(openItem);
 file.append(saveItem);
 file.append(saveAsItem);
+file.append(closeItem);
+
 win.menu=menubar;
 win.menu.insert(new gui.MenuItem({ label: 'File', submenu: file}), 1);
 
+newItem.click=function(){
+	newFile();
+};
 openItem.click=function(){
-  	openFile();
-}
+	openFile();
+};
 saveAsItem.click=function(){
 	saveFileAs();
-}
+};
 saveItem.click=function(){
 	saveFile();
+};
+closeItem.click = function(){
+	close();
+};
+
+function newFile(){
+	$("#attacker").val("").trigger("change");
+	currentfile = null;
 }
 
 function openFile() {
     var chooser = $("#opendlg");
     chooser.change(function(evt) {
-    	var fileName=$(this).val();
+		var fileName=$(this).val();
 		fs.exists(fileName, function(exists) {
 		if (exists) {
 		fs.stat(fileName, function(error, stats) {
-		  fs.open(fileName, "r", function(error, fd) {
-		    var buffer = new Buffer(stats.size);
+			fs.open(fileName, "r", function(error, fd) {
+			var buffer = new Buffer(stats.size);
 
-		    fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-		      var data = buffer.toString("utf8", 0, buffer.length);
-		      $("#attacker").val(data).trigger("change");
-		      currentfile=fileName;
-		      fs.close(fd);
+			fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+				var data = buffer.toString("utf8", 0, buffer.length);
+				$("#attacker").val(data).trigger("change");
+		    	currentfile=fileName;
+		    	fs.close(fd);
 		    });
 		  });
 		});
@@ -48,7 +66,7 @@ function openFile() {
 		});
     });
     chooser.trigger('click');  
- }
+ };
 
  function saveFile(){
  	if(currentfile!=null)
@@ -64,10 +82,15 @@ function saveFileAs(){
     });
     chooser.trigger('click');  
 }
+
+function close(){
+	win.close();
+}
+
 function writeFile(path, content){
 	fs.writeFile(path, content, function(err) {
-	    if(err) {
-	        alert("Error writing file!");
-	    }
+		if(err) {
+			alert("Error writing file!");
+		}
 	});
 }
