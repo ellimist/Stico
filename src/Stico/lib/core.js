aaa4389d393c87a803e717459405716cc3499ebd
andrew <andrew@it-kartellet.dk>
2013-09-07 02:08:54 +0200

    implemented open, save and save as

--- src/Stico/lib/core.js
+++ src/Stico/lib/core.js
@@ -1,24 +1,77 @@
-$(function(){
-	$('body').delegate('#attacker', 'keyup change', function(){
-		runmd();
-	});
+/* my globals */
+var currentfile=null;
+
+var gui = require('nw.gui');
+var fs = require('fs');
+
+var Menu = new gui.Menu({
+    type:   'menubar'
 });
+var FileMenu = new gui.Menu();
+var openItem=new gui.MenuItem({label: 'Open'});
+var saveItem=new gui.MenuItem({label: 'Save'});
+var saveAsItem=new gui.MenuItem({label: 'Save As'});
+FileMenu.append(openItem);
+FileMenu.append(saveItem);
+FileMenu.append(saveAsItem);
+Menu.append(
+    new gui.MenuItem({
+        label: 'File',
+        submenu: FileMenu
+    })
+);
+gui.Window.get().menu = Menu;
 
-function runmd(){
-	//vars
-	var output=null;
-	var input=null;
+openItem.click=function(){
+  	openFile('#opendlg');
+}
+saveAsItem.click=function(){
+	saveFileAs("#savedlg");
+}
+saveItem.click=function(){
+	saveFile();
+}
 
-	//get current input
-	input=$("#attacker").val();
-	output=input;
+function openFile(name) {
+    var chooser = $(name);
+    chooser.change(function(evt) {
+    	var fileName=$(this).val();
+		fs.exists(fileName, function(exists) {
+		if (exists) {
+		fs.stat(fileName, function(error, stats) {
+		  fs.open(fileName, "r", function(error, fd) {
+		    var buffer = new Buffer(stats.size);
 
-	//parse input
-	output=markdown.toHTML(input);
+		    fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+		      var data = buffer.toString("utf8", 0, buffer.length);
+		      $("#attacker").val(data).trigger("change");
+		      currentfile=fileName;
+		      fs.close(fd);
+		    });
+		  });
+		});
+		}
+		});
+    });
+    chooser.trigger('click');  
+ }
 
-	//publish to #preview
-	var doc=$("#victim")[0].contentWindow.document;
-	doc.open();
-	doc.write(output);
-	doc.close();
+ function saveFile(){
+	writeFile(currentfile, $("#attacker").val());
+}
+function saveFileAs(name){
+	var chooser = $(name);
+    chooser.change(function(evt) {
+    	var fileName=$(this).val();
+		writeFile(fileName, $("#attacker").val());
+		currentfile=fileName;
+    });
+    chooser.trigger('click');  
+}
+function writeFile(path, content){
+	fs.writeFile(path, content, function(err) {
+	    if(err) {
+	        alert("Error writing file!");
+	    }
+	});
 }
